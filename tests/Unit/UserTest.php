154ee2f93c9e588e1ce5d3a6bb93645984c5a0fd
PhucDuong-SSS <phucdnDev@gmail.com>
2021-04-20 17:27:27 +0700

    day333

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -30,12 +30,57 @@ class UserTest extends TestCase
     {
         $userService = Mockery::mock(UserService::class);
         $user= new UserController($userService);
-        $userService->shouldReceive('destroy')
-            ->withArgs([100])
-            ->andReturn([]);
+        $userService->shouldReceive('delete')
+            ->withArgs([1])
+            ->andReturn(true);
         $actual = $user->destroy(1);
 
-        $this->assertSame([], $actual);
+        $this->assertEquals(true, $actual);
     }
+
+    public function testStoreUsers()
+    {
+        $userService = Mockery::mock(UserService::class);
+        $user= new UserController($userService);
+        $userService->shouldReceive('store')
+            ->andReturn(true);
+        $actual = $userService->store(['user_name'=>'phuc','email'=>'phuc@gmail.com']);
+
+        $this->assertEquals(true, $actual);
+    }
+    public function testUpdateUsers()
+    {
+        $userService = Mockery::mock(UserService::class);
+
+        $userService->shouldReceive('update')
+            ->andReturn(true);
+        $user= new UserController($userService);
+        $actual = $userService->update(['user_name'=>'phuc','email'=>'phuc@gmail.com'],1);
+
+        $this->assertEquals(true, $actual);
+    }
+    public function testGetUserHasMostBlogs()
+    {
+        $userService = Mockery::mock(UserService::class);
+
+        $userService->shouldReceive('getUserHasMostBlogs')
+            ->andReturn(true);
+        $user= new UserController($userService);
+        $actual = $user->getUserHasMostBlogs();
+
+        $this->assertEquals(true, $actual);
+    }
+    public function testGetUserHasLeastBlogs()
+    {
+        $userService = Mockery::mock(UserService::class);
+
+        $userService->shouldReceive('getUserHasLeastBlogs')
+            ->andReturn(true);
+        $user= new UserController($userService);
+        $actual = $user->getUserHasLeastBlogs();
+
+        $this->assertEquals(true, $actual);
+    }
+
 
 }
